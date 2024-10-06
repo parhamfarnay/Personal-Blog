3a3654f647a79f9e42da172032dbeb6e352eb542
parhamfarnay <prhmfrny@gmail.com>
2024-10-07 01:50:12 +0330

    feat/queries handled and cards created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
 import Header from "./components/layout/Header";
 import HomePage from "./components/home/HomePage";
+import Layout from "./components/layout";
 
 const QUERY = gql`
   query {
@@ -15,10 +16,9 @@ function App() {
   console.log(data);
 
   return (
-    <>
-      <Header />
+    <Layout>
       <HomePage />
-    </>
+    </Layout>
   );
 }
 
