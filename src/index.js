0b563c573c29309f6e1773c020b370db0083f10a
parhamfarnay <prhmfrny@gmail.com>
2024-10-06 15:45:04 +0330

    feat/material ui customized in mui folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import theme from "./mui/theme";
+import App from "./App";
+
 import "./styles/index.css";
 import "./styles/fonts.css";
-import App from "./App";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://ap-south-1.cdn.hygraph.com/content/cm1q5e8iw02bk08wbhx6j0bx7/master",
@@ -13,6 +18,10 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </ApolloProvider>
 );
